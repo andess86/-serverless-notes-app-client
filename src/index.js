1e552947db9704e964619f36855547a739da9c1c
Anders Svensson <andess86@gmail.com>
2020-03-05 16:26:07 +0100

    Set up AWS Amplify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Amplify from 'aws-amplify';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
