df004312aa5124d7b51d97a5beac27f3d4a0306a
Anders Svensson <andess86@gmail.com>
2020-03-06 16:34:41 +0100

    Add AppliedRoute-component

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import { Auth } from "aws-amplify";
-import "./Login.css";
+import React, { useState } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { Auth } from 'aws-amplify';
+import './Login.css';
 
 export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -13,10 +13,10 @@ export default function Login(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     try {
       await Auth.signIn(email, password);
-      alert("Logged in");
+      props.userHasAuthenticated(true);
     } catch (e) {
       alert(e.message);
     }
