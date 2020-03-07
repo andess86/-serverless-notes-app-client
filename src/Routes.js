84d49ecfde4ded69e3e32a7e426b9121232167cc
Anders Svensson <andess86@gmail.com>
2020-03-07 08:36:53 +0100

    Add New Note & Signup containers

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from "./components/AppliedRoute";
 import Home from './containers/Home';
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
@@ -11,6 +13,8 @@ export default function Routes({ appProps }) {
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
