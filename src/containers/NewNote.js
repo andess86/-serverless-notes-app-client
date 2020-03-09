31334dfb7813372ceda2a6f701dadb19a867ae14
Anders Svensson <andess86@gmail.com>
2020-03-09 09:06:09 +0100

    Add create note flow

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,7 +1,9 @@
 import React, { useRef, useState } from "react";
+import { API } from "aws-amplify";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
+import { s3Upload } from "../libs/awsLib";
 import "./NewNote.css";
 
 export default function NewNote(props) {
@@ -19,7 +21,7 @@ export default function NewNote(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
+  
     if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
         `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
@@ -27,8 +29,26 @@ export default function NewNote(props) {
       );
       return;
     }
-
+  
     setIsLoading(true);
+  
+    try {
+      const attachment = file.current
+        ? await s3Upload(file.current)
+        : null;
+  
+      await createNote({ content, attachment });
+      props.history.push("/");
+    } catch (e) {
+      alert(e);
+      setIsLoading(false);
+    }
+  }
+  
+  function createNote(note) {
+    return API.post("notes", "/notes", {
+      body: note
+    });
   }
 
   return (
