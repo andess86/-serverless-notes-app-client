97a401669437f6a5bc58bf6afa2b64df41b6cfe9
Anders Svensson <andess86@gmail.com>
2020-03-09 09:25:05 +0100

    Add edit / delete flow

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from './containers/Home';
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
@@ -15,6 +16,7 @@ export default function Routes({ appProps }) {
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
       <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
